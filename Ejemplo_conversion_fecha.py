585a14283458a8a1a68c193d73a4b6a7dde8cd53
lupera13k <kevinlupera1997@outlook.com>
2018-01-26 16:35:29 -0500

    V1
    Local Data

--- Ejemplo_conversion_fecha.py
+++ Ejemplo_conversion_fecha.py
@@ -1,17 +1,24 @@
-import time
+#import time
 import pandas as pd
-from datetime import date
+from datetime import date 
 
 datos = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
      "capital": ["Brasilia", "Moscow", "New Dehli", "Beijing", "PretoriaDDDD"],
      "date": [1464010200, 1464010201, 1464010202, 1464010203, 1464010204] }
 df = pd.DataFrame(datos)
-ldate=[]
-# Cambiar formato de fechas
-ldate= pd.to_datetime(df['date'], format='%Y-%m-%d')
+#print(df['date'][0])
+#x = date.fromtimestamp(df['date'][0]).strftime('%Y-%m-%d %H:%M')
+#print(x)
+list1=[]
+#Cambiar formato de fechas
+#list1= pd.to_datetime(df['date'], format='%Y-%m-%d')
 for i in range(len(ldate)):
-    ldate[i]=ldate[i].strftime('%Y-%m-%d')
-df['date']=ldate
+    x = date.fromtimestamp(df['date'][i]).strftime('%Y-%m-%d')
+    list1.append(x)
+#df['date']=df['date'].apply(lambda x: dt.datetime.strptime(x,'%d%b%Y:%H:%M:%S.%f'))
+print(list1)
+"""
+#df['date']=ldate
 #1 Agregar la fecha como indice
 dfOri=pd.DataFrame(datos,index=ldate)
 #Eliminar la columna date
@@ -22,3 +29,4 @@ dfOri=pd.DataFrame(datos)
 dfOri.date=ldate
 #Imprimir resultados
 print(dfOri)
+"""
--- tarea.py
+++ tarea.py
@@ -2,38 +2,51 @@
 """
 Created on Mon Jan 22 18:15:05 2018
 
-@author: kevin
+@author: kevin lupera
 """
 
 import pandas as pd
 from datetime import date
-
-    
+import numpy as np
+#crear dataFrame de archivos    
 def create_df(file_name):
     quotes = []
     list1=[]
     if file_name=="u.data":
-        list1 = ['id_usuario','id_elemento','calificacion','date']
+        list1 = ['id_usu','id_mov','calificacion','fecha']
         quotes=pd.read_table (file_name, sep = '\t', names = list1)
     elif file_name=="u.user":
-        list1 = ['id_usuario','edad','genero','ocupacion','codigo postal']
+        list1 = ['id_usu','edad','genero','ocupacion','cod_postal']
         quotes=pd.read_table (file_name, sep = '|', names = list1)
-    elif file_name=="u.item":
-        list1 = ['identificación de la película', 'título de la película', 'fecha de lanzamiento', 'fecha de lanzamiento del video', 'IMDb URL', 'desconocido ', 'Acción', 'Aventura', 'Animación', 'Niños','Comedia', 'Crimen', 'Documental', 'Drama', 'Fantasía','Película-Noir' , 'Horror' , 'Musical Misterio' , 'Romance' , 'Ciencia ficción' , 'Novela de suspense', ' Guerra ', 'Western ']
+    elif file_name=="item.txt":
+        list1 = ['id_mov','título_película', 'fecha_lanz', 'fecha_lanz_video', 'IMDb URL', 'desconocido ', 'Acción', 'Aventura', 'Animación', 'Niños','Comedia', 'Crimen', 'Documental', 'Drama', 'Fantasía','Humor_negro' , 'Horror' , 'Musical' ,'Misterio', 'Romance' , 'Ciencia_ficción' , 'Suspenso', ' Guerra ', 'Western ']
         quotes=pd.read_table (file_name, sep = '|', names = list1)
-    #pd.pivot_table (data, values ​​= None, columns = None, aggfunc = 'mean')
-    #pd.merge (left, right, cómo = 'interior')
     dataFrame = pd.DataFrame(quotes,columns=list1)
-    list2 = []
-    for i in range(len(dataFrame)):
-        x = date.fromtimestamp(dataFrame[i]['date'])
-        y = date.strftime(x,'%Y-%m-%d')
-        list2.append(y)
-    quotesdf_ori = pd.DataFrame(dataFrame, index = list2)
-    quotesdf = quotesdf_ori.drop(['date'], axis = 1)
     return dataFrame
-
-
-print(create_df("u.data"))
-#print(create_df("u.user"))
-#print(create_df("u.item"))
+#convertir formato de fecha
+def fecha_df(df):
+    list1=[]
+    # Cambiar formato de fechas
+    for i in range(len(df)):
+        x = date.fromtimestamp(df['fecha'][i]).strftime('%Y-%m-%d')
+        list1.append(x)
+    df['fecha']=list1
+    #Reemplazar el campo date con la fecha en un nuevo formato
+    dfOri=pd.DataFrame(df)
+    dfOri.date=list1
+    return dfOri
+#obtener informacion de los archivos y crear los DataFrames
+data=fecha_df(create_df("u.data"))
+user=create_df("u.user")
+item=create_df("item.txt")
+#Agrupar la informacion por genero
+table = pd.pivot_table (user, columns = 'genero', aggfunc = np.mean)
+#calcular la desviacion estandar de la calificacion
+desv_est=np.std(user['edad'])
+#print(desv_est)
+#Fusionar las columnas 
+a=data.merge(user,on='id_usu' , how='inner')
+b=a.merge(item, on='id_mov', how='inner')
+#agrupar
+resultado = pd.pivot_table (b, values='calificacion',columns = 'genero', aggfunc = np.mean)
+print(resultado)
